167167b83fa14e0a7cd208e52bdc4fcee7f67821
Stas <staslz42stas@gmail.com>
2019-12-12 17:11:14 +0300

    RGB!!!

--- src/common.h
+++ src/common.h
@@ -8,13 +8,26 @@
  #endif
 #include <EEPROM.h>
 #include "GyverTimer.h"
-
+/*
+place
+#define work
+//#define Lz428266ZV // Zvonok
+//#define Lz428266YE // YELLOW
+//#define Lz428266VFD // VFD
+#define Lz428266WR // work, uno+8266
+*/
 #undef Serialmy
 #define updint 1000
 
 GTimer_ms Tticktime(updint);
 GTimer_ms Ttickclock(250);
 GTimer_ms Treboot(250000);
+// 15 RED, 14 - рядом маленький 16 - рядом ещё ближе  13 - синий 12 зелёный
+#ifdef Lz428266YE
+ #define redpin 15
+ #define bluepin 13
+ #define greenpin 12
+#endif
 
 uint8_t hour;
 uint8_t mins;
@@ -26,6 +39,31 @@ int year;
 bool syncgood = false;
 
 volatile int cnt = 0;
+
+void Button(int b){
+ int state;
+ switch(b){
+  case 0: 
+    #ifdef redpin
+     digitalRead(redpin, state);
+     digitalWrite(redpin,!state);
+    #endif
+    break;
+  case 1: 
+    #ifdef bluepin
+     digitalRead(bluepin, state);
+     digitalWrite(bluepin,!state);
+    #endif
+    break;
+  case 2: 
+    #ifdef greenpin
+     digitalRead(greenpin, state);
+     digitalWrite(greenpin,!state);
+    #endif
+    break;
+ }
+
+ }
 
 void beep(int t1,int t2){
   #ifdef pinbeep
@@ -54,5 +92,14 @@ void initCommon(void){
   #ifdef pinbeep
     pinMode(pinbeep,OUTPUT);
    #endif
+  #ifdef redpin
+    pinMode(redpin,OUTPUT);
+   #endif
+  #ifdef bluepin
+    pinMode(bluepin,OUTPUT);
+   #endif
+  #ifdef greenpin
+    pinMode(greenpin,OUTPUT);
+   #endif   
  }
 
