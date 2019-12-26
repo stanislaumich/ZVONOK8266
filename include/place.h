2b15a78caffad4855acbe3c10e12e50890b04485
Stas <staslz42stas@gmail.com>
2019-12-26 14:59:53 +0300

    перенес инклуды

---
+++ include/place.h
@@ -0,0 +1,5 @@
+#define work
+//#define Lz428266ZV // Zvonok          ok, work
+//#define Lz428266YE // YELLOW          ok, work
+//#define Lz428266VFD // VFD            ok, work
+#define Lz428266WR // work, uno+8266, ok, work
